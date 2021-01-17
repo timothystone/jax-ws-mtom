f14015927e5e0650dc7403845b68773243ca6bbe
Timothy Stone <github@petmystone.com>
2021-01-17 09:17:54 -0500

    Create the CXF for JAX-WS MTOM project
    Initial commit of all files generated by Spring Initializr

---
+++ src/test/java/com/anothercaffeinatedday/ws/soap/mtom/MtomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.ws.soap.mtom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtomApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
